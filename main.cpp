d49bc6b4a825e951f6ee412600e252ab6481694d
Retriever <xphuangsg@gmail.com>
2017-01-03 14:22:55 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,106 @@
+//
+//  main.cpp
+//  Programming in C++
+//
+//  Created by 快耍慢活 on 3/1/17.
+//  Copyright © 2017 sheldon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    //T10
+    int a[5] = {2,3,56,76,92};
+    for( int i = 0; i < 5; i++){
+        std::cout << "Number #"<< i + 1 << ":" << a[i] << '\n';
+    }
+    
+    
+    
+    
+    
+    //T9 for statement
+//    int a;
+//    for(a = 0 ; a < 5; a++){ //initialization; condition; increment
+//        std::cout << "Number" << '\n';
+//    }
+    
+    
+    //T8 Switch
+//    int a = 7;
+//    switch (a){
+//        case 1:
+//            std::cout << "one \n";
+//            break;
+//        case 2:
+//            std::cout << "two \n";
+//            break;
+//        case 3:
+//            std::cout << "three \n";
+//            break;
+//            
+//        default:
+//            std::cout << "Error \n";
+//            break;
+//    }
+    return 0;
+    
+    
+    
+    //T5//T6 //T7 if else
+    
+//        short int a;
+//        std::cout << "Enter Amount....."; //standard output stream
+//        std::cin >> a;                    //standard input stream
+//    
+//        std::cout  << "The Amount Your entered was.." <<a << "\n";;
+//    
+//        if(a == 5)
+//            std::cout << "CORRECT!!!" << "\n";
+//        else if(a<=5)
+//             std::cout << "Too SMALL!!!" << "\n";
+//        else
+//             std::cout << "WRONG!!!" << "\n";
+    
+//    //T4
+//    char a[20];
+//    std::cout << "Enter a name....";
+//    std::cin >> a;
+//    
+//    std::cout << "The Name your entered was:";
+//    std::cout << "\n";
+//    
+    
+    return 0;
+    
+    
+    
+    //T3
+//    short int a;
+//    std::cout << "Enter Amount....."; //standard output stream
+//    std::cin >> a;                    //standard input stream
+//    
+//    std::cout  << "The Amount Your entered was.." <<a << "\n";;
+
+    
+    
+    
+    // T2
+//    int a = 12;
+//    float b = 1.234;
+//    char c = 'C';
+//    bool d = true;
+//    std::cout << a << ' ';
+//    std::cout << b << ' ';
+//    std::cout << c << ' ';
+//    if(d)
+//        std::cout << "true" << " \n";
+    
+    
+    
+//    std::cout << "Hello, World!\n";
+//    return 0;
+}
